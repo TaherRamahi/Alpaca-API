1295377f8af18a6484ccad567759c62af122d625
TaherRamahi <taherramahi806@gmail.com>
2023-12-18 02:08:09 -0600

    We managed to get the acct info using a GET request

--- trade.py
+++ trade.py
@@ -2,6 +2,12 @@ import requests
 from config import *
 BASE_URL = "https://paper-api.alpaca.markets"
 
-r = requests.get(BASE_URL)
+
+#This is to see acct info we are using a placeholder and using the .format() to create a link that we can use to request acct data. VID 8:42
+Acount_URL = "{}/v2/account".format(BASE_URL)
+acctInfo = requests.get(Acount_URL)
+
+#For this API we need to pass in our KEY and SECRET KEY when we attempt to make a call to the API using any URL. The KEY and SaECRET key are passed into the header dict which I am still not sure how it works.
+r = requests.get(Acount_URL, headers={ 'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY' : SECRET_KEY })
 
 print(r.content)
