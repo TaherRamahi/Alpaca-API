4638468d80616504c3b184916ea6cb4bf51134a3
TaherRamahi <taherramahi806@gmail.com>
2024-01-25 18:50:17 -0600

    Created the ability to create orders (buy/sell) and list orders

--- trade.py
+++ trade.py
@@ -16,16 +16,33 @@ def getAcctInfo():
     return json.loads(r.content)
 
 #The args are all required by the API to create an order
+# $$$ This function allows us to buy shares with an  API call, we just input the arguments necessary in the function call and run the trade.py file in the terminal
 def createOrder(symbol, qty, side, type, time_in_force):
     # the request to the API is going to be sent in a dict that i named data VID: 12.00
     data = {
-
+    "symbol": symbol,
+    "qty": qty,
+    "side": side,
+    "type": type ,
+    "time_in_force": time_in_force
 
 
     }
 
 
-    r = requests.post(ORDERS_URL, headers= HEADERS)
+    r = requests.post(ORDERS_URL, json = data, headers= HEADERS)
     
     return json.loads(r.content)
-        
+
+# $$$ we make requests such as this one bellow to buy/sell shares and the comments bellow can be used to make an order and we print the response to the terminal in the line under it.
+# response = createOrder("AAPL", 100,"buy","market","gtc")
+#print(response)
+
+# This Function  serves to get all current orders from the API
+def getOrders():
+   r = requests.get(ORDERS_URL, headers= HEADERS)
+   return json.loads(r.content)
+# This below is actully running the get orders function and  printing to the terminal
+ordersPending = getOrders()
+print(ordersPending)
+
